673bfae4cd205ed1877dc00c646844df80abdb3e
RydertHuGlIfE <varunchauhan2001dma@gmail.com>
2026-03-23 12:03:34 +0530

    initial backedn mongodb commit

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import os
+import dotenv
+from flask_pymongo import PyMongo
+
+dotenv = dotenv.load_dotenv()
+monguri = os.getenv('MONGO_URI')
 
 app = Flask(__name__)
 # Enable CORS for the frontend origin
+app.config['MONGO_URI'] = monguri
+mongo = PyMongo(app)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
 @app.route('/api/status', methods=['GET'])
@@ -11,6 +19,22 @@ def get_status():
         "status": "healthy",
         "message": "Connected to Flask Backend",
         "version": "1.0.0"
+    })
+
+
+@app.route("/")
+def home_page():
+    try:
+        mongo.cx.admin.command('ping')
+        db_status = "ONLINE"
+    except Exception as e:
+        db_status = "OFFLINE"
+    
+    return jsonify({
+        "status": "healthy",
+        "message": "Connected to Flask Backend",
+        "version": "1.0.0",
+        "db_status": db_status
     })
 
 if __name__ == '__main__':
